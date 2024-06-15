6464bcafa5cf7dfce0f435ef6b1c614f48007e23
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-06-16 00:03:07 +1000

    Engine:  - Add option Configuration store  - Add Options customisation page

---
+++ src/stores/configuration.ts
@@ -0,0 +1,80 @@
+import { defineStore } from 'pinia'
+
+export const labelMap_EN = new Map<String, String>();
+labelMap_EN.set('audio', 'Audio');
+labelMap_EN.set('bgm'  , 'Music');
+labelMap_EN.set('sfx'  , 'Effects');
+labelMap_EN.set('voice', 'Voices');
+
+const KEY = 'illusionEngine_config';
+const CONFIG_KEYS = Object.freeze([
+  'audio',
+  'bar',
+  'baz',
+]);
+
+export interface IConfiguration {
+  audio: IAudioConfiguration,
+  bar: String,
+  baz: String,
+};
+
+export interface IAudioConfiguration {
+  bgm: number,
+  sfx: number,
+  voice: number,
+}
+const DEFAULT_STATE: IConfiguration = Object.freeze({
+  audio: {
+    bgm: 0.7,
+    sfx: 0.7,
+    voice: 0.7,
+  },
+  bar: '',
+  baz: '',
+});
+
+function pluckKeys(collection: {[x:string]: any}, keys: string[] | readonly string[]){
+  const retval: {[x:string]: any} = {};
+
+  keys.forEach((key) => {
+    if (collection.hasOwnProperty(key)){
+      if (typeof collection[key] === 'object'){
+        retval[key] = {...collection[key]};
+      } else if (Array.isArray(collection[key])) {
+        retval[key] = [...collection[key]];
+      } else {
+        retval[key] = collection[key];
+      }
+    }
+  });
+
+  return retval;
+}
+
+export const useConfiguration = defineStore('configuration', {
+  state: () => {
+    return {...DEFAULT_STATE}
+  },
+
+  actions: {
+    init() {
+      const existingSettings = window.localStorage.getItem(KEY) || null;
+      if (existingSettings){
+        console.log(existingSettings)
+      }
+    },
+    save() {
+      const localInstance = pluckKeys({...this}, CONFIG_KEYS);      
+      window.localStorage.setItem(KEY, JSON.stringify(localInstance));
+    },
+    update(accessPath: string, value: any){
+
+      // this['audio']
+      // this.audio
+    },
+    getConfigurables(){
+      return DEFAULT_STATE;
+    },
+  },
+})
---
+++ src/views/menus/Options.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+  import { nextTick, ref } from 'vue'
+  import videoSrc from '@assets/bg-audio.mp4';
+  import { useConfiguration, IConfiguration, IAudioConfiguration, labelMap_EN } from '@stores/configuration';
+  import { 
+    useBgmEngine, 
+    useSfxEngine,
+    useVoiceEngine,
+  } from '&audio'
+
+  const bgmEngine = useBgmEngine()
+  const sfxEngine = useSfxEngine()
+  const voiceEngine = useVoiceEngine()
+
+  const config = useConfiguration()
+
+  const configurables = config.getConfigurables();
+  const localAudio = ref<IAudioConfiguration>(configurables.audio);
+
+  function isItterable(thing: any): boolean{
+    if (Array.isArray(thing)){
+      return true;
+    } else if (typeof thing === 'object'){
+      return true;
+    }
+
+    return false;
+  }
+
+  function resolveValue(thing: any, key: string): any{
+    let retval = '';
+
+    try {
+      retval = thing[key];
+    }catch{
+      console.log(`unable to resolve value for key: '${key}' in collection: ${thing}`);
+    }
+
+    return retval;
+  }
+
+  function resolveLabel(key: string) {
+    if (! labelMap_EN.has(key)) return key;
+    return labelMap_EN.get(key);
+  }
+
+  function saveOnUpdate(audioKey: string){
+    const key =  audioKey as keyof IAudioConfiguration;
+    nextTick(() => {
+      switch(key){
+        case('bgm'):
+            bgmEngine.setVolume(configurables.audio[key]);
+          break;
+        case('sfx'):
+          sfxEngine.setVolume(configurables.audio[key]);
+          break;
+        case('voice'):
+          voiceEngine.setVolume(configurables.audio[key]);
+          break;
+        default:
+      }
+    })
+  }
+
+  function testAudio(audioKey: string){
+    const key =  audioKey as keyof IAudioConfiguration;
+      switch(key){
+        case('sfx'):
+          sfxEngine.setAndPlay('/audio/sfx/ah.wav');
+          break;
+        case('voice'):
+          voiceEngine.setAndPlay('/audio/voice/yo-da-yo.wav');
+          break;
+        default:
+      }
+  }
+
+</script>
+
+<template>
+  <div class="animate-fadeIn">
+    <div class='grid [grid-template-areas:_"stack"] *:[grid-area:_stack]'>
+      <section class='aspect-video pointer-events-none'>
+        <video
+          autoplay
+          loop
+          muted
+          playsinline
+        >
+          <source :src="videoSrc" type="video/mp4">
+        </video>
+      </section>
+      <div class='mx-8 my-4 p-4 aspect-video z-10 bg-slate-500/50 rounded-xl'>
+        <section class="flex justify-between">
+          <h1>Options</h1>
+          <span class="flex gap-2">
+            <RouterLink to="/menu"><h1>Save</h1></RouterLink>
+            <RouterLink to="/menu"><h1>Back</h1></RouterLink>
+          </span>
+        </section>
+        <section class='grid grid-cols-4'>
+          <div v-for="(key) in Object.keys(configurables)">
+            <h2 class="text-xl">{{resolveLabel(key)}}</h2>
+            <template v-if="isItterable(configurables[key as keyof IConfiguration])">
+              <template v-if="key === 'audio'">
+                <div v-for="(el) in Object.keys(configurables[key as keyof IConfiguration])" class="flex justify-around mx-4">
+                  <div class="flex justify-between min-w-32">
+                    <span>{{ resolveLabel(el) }}</span>
+                    <span>{{ Math.floor(resolveValue(configurables[key as keyof IConfiguration], el) * 100) }}%</span>
+                  </div>
+                  <input 
+                    type="range"
+                    id="volume"
+                    name="volume"
+                    min="0" 
+                    max="1" 
+                    step="0.05"
+                    v-model.number="localAudio[el as keyof IAudioConfiguration]"
+                    @input="() => saveOnUpdate(el)"
+                  />
+                  <button @click="testAudio(el)" :disabled="el === 'bgm'">Test</button>
+                </div>
+              </template>
+            </template>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
