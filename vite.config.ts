d442aeef4e564bfd213429bac6ad9c1e66bf28e4
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-06-16 00:00:51 +1000

    DX:  - Add aliases for base areas

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  publicDir: "public",
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@stores": path.resolve(__dirname, "./src/stores"),     
+      "&audio": path.resolve(__dirname, "./src/stores/audio"),      
+    },
+  },
   plugins: [vue()],
 })
