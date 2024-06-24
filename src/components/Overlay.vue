a8ff3a492144f40d262a0ec115864271f9e517a5
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-06-24 22:59:44 +1000

    Engine:  - Added Scene Player structure  -- Added hooks for when state progresses and we need to be reactive relevent to the new mutated state.  - First pass on text-overlay layer

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -1,3 +1,63 @@
-<template>
+<script setup lang="ts">
+  import { useScriptEngine } from '@stores/scriptEngine';
+  import { ref } from 'vue';
 
+  const isAuto = ref(false);
+  const isViewBackdrop = ref(false);
+  const isPoked = ref(false);
+
+  const scriptEngine = useScriptEngine()
+
+  function progress(){
+    scriptEngine.progress();
+  }
+  function reset(){
+    scriptEngine.reset();
+  }
+
+
+  
+
+  function autoToggle(){
+    isAuto.value = !isAuto.value;
+  }
+  function skipToggle(){
+    console.log('Skipping');
+  }
+  function viewBackdropToggle(){
+    isViewBackdrop.value = !isViewBackdrop.value;
+  }
+  function poke(){
+    isPoked.value = !isPoked.value;
+  }
+
+</script>
+
+<template>
+  <section class='flex flex-col justify-between px-8 py-4 aspect-video z-10'>
+    <div class='flex justify-between px-8 py-4'>
+      <section>
+        <button v-show="!isViewBackdrop" @click="skipToggle()" class="px-4 py-2 bg-white text-black rounded h-fit" >Skip</button>
+      </section>
+      <section class='flex justify-between px-8 py-4'>
+        <button v-show="!isViewBackdrop" @click="autoToggle()" class="px-4 py-2 bg-white text-black rounded h-fit" >{{isAuto ? 'Auto' : 'Pause'}}</button>
+        <button @click="viewBackdropToggle()" class="px-4 py-2 bg-white text-black rounded h-fit">{{isViewBackdrop ? 'show' : 'hide'}}</button>
+      </section>
+    </div>
+    <div v-show="!isViewBackdrop" class="min-h-[20%]">
+      <section class='flex flex-col items-center px-8 py-4 h-full'>
+        <button @click="poke()" class="px-4 py-2 bg-white text-black rounded h-fit">poke</button>
+        <h3 class="text-2xl ">Speaker</h3>
+        <p :class="[
+          'inline transition-all ease-linear duration-[2000ms] text-white/0',
+          'bg-0_100',
+          'text-white/0',
+          'bg-gradient-to-r from-white to-white bg-no-repeat bg-clip-text',
+          { '!bg-100_100' : isPoked },
+          ]">Here there are alot of words, Here there are alot of words, Here there are alot of words, Here there are alot of words</p>
+      </section>
+    </div>
+    <!-- <button @click="progress()" class="px-4 py-2 bg-white text-black rounded h-fit">Progress</button>
+    <button @click="reset()" class="px-4 py-2 bg-white text-black rounded h-fit">Reset</button> -->
+  </section>
 </template>
--- src/views/ScenePlayer.vue
+++ src/views/ScenePlayer.vue
@@ -1,6 +1,56 @@
 <script setup lang="ts">
   import videoSrc from '@assets/bg-audio.mp4';
   import Backdrop from '@components/Backdrop.vue';
+  import Overlay from '@components/Overlay.vue';
+  import { useScriptEngine } from '@stores/scriptEngine';
+
+  const scriptEngine = useScriptEngine()
+
+  scriptEngine.$onAction((el)=> {
+    const startTime = Date.now()
+    el.onError((error) => {
+      console.warn(
+        `Failed "${el.name}" after ${Date.now() - startTime}ms.\nError: ${error}.`
+      )
+    })
+
+    if (el.name === '_updateBgm'){
+      console.log(el);
+      el.after((result) => {
+        console.log('update BGM: \t' + JSON.stringify(el.args));
+      })
+    }
+
+    if (el.name === '_updateBackdrop'){
+      console.log(el);
+      el.after((result) => {
+        console.log('update Backdrop: \t' + JSON.stringify(el.args));
+      })
+    }
+
+    if (el.name === '_updateChars'){
+      console.log(el);
+      el.after((result) => {
+        console.log('update Chars: \t' + JSON.stringify(el.args));
+      })
+    }
+
+    if (el.name === '_updateText'){
+      console.log(el);
+      el.after((result) => {
+        console.log('update Text: \t' + JSON.stringify(el.args));
+      })
+    }
+
+    if (el.name === '_updateTransitions'){
+      console.log(el);
+      el.after((result) => {
+        console.log('update Transitions: \t' + JSON.stringify(el.args));
+      })
+    }
+
+  });
+
 
 </script>
 
@@ -8,15 +58,15 @@
   <div class="animate-fadeIn">
     <div class='grid [grid-template-areas:_"stack"] *:[grid-area:_stack]'>
       <!-- Backdrop -->
-       <Backdrop
+      <Backdrop
         :isVideo="true"
         :contentSrc="videoSrc"
         contentType="video/mp4"
       />
       <!-- Interactions / char layer -->
-       <section>foooo</section>
+      <section>foooo</section>
       <!-- Overlay -->
-       <section>baaaaar</section>
+      <Overlay />
     </div>
   </div>
 </template>
