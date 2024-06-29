20471a2be9dd84a65d70f6b78fcbbfbd7648dff7
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-06-29 14:36:28 +1000

    Engine:  - Add skip icon

---
+++ src/components/icon/skip.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg" 
+    width="36"
+    height="24"
+    viewBox="0 0 36 24"
+    fill="none"
+    stroke="white"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-skip-forward"
+  >
+    <polygon points="6 4 17 12 6 20 6 4"></polygon>
+    <polygon points="17 4 29 12 17 20 17 4"></polygon>
+    <line x1="30" y1="5" x2="30" y2="19"></line>
+  </svg>
+</template>
