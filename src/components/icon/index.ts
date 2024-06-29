14542a1f3e545a3fb0158857f574afaf8b98c16f
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-06-29 19:15:15 +1000

    Engine:  - Add icons for volumen and loader  - Add  DX index for importing

---
+++ src/components/icon/index.ts
@@ -0,0 +1,15 @@
+import EyeIcon from './eye.vue';
+import BookIcon from './book-open.vue';
+import LoaderIcon from './loader.vue';
+import RefreshIcon from './refresh-cw.vue';
+import SkipIcon from './skip.vue';
+import VolumeIcon from './volume.vue';
+
+export { 
+  EyeIcon,
+  BookIcon,
+  LoaderIcon,
+  RefreshIcon,
+  SkipIcon,
+  VolumeIcon,
+};
