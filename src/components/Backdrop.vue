8c0d970f86b677a8130e013faf8e65a17343c4fc
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-07-02 21:33:01 +1000

    Engine:  - Add support for transitioning bg's ontop of each other

--- src/components/Backdrop.vue
+++ src/components/Backdrop.vue
@@ -12,16 +12,34 @@
 </script>
 
 <template>
-  <section class='aspect-video pointer-events-none'>
-    <video
-      v-if="props.isVideo"
-      autoplay
-      loop
-      muted
-      playsinline
-    >
-      <source :src="props.contentSrc" :type="props.contentType">
-    </video>
-    <img v-else :src="props.contentSrc">
+  <section class="aspect-video pointer-events-none min-w-[min(100vw,1920px)]">
+    <Transition>
+      <video
+        v-if="props.isVideo"
+        autoplay
+        loop
+        muted
+        playsinline
+        class="fixed"
+      >
+        <source :src="props.contentSrc" :type="props.contentType">
+      </video>
+      <img 
+        v-else 
+        :src="props.contentSrc" 
+        class="fixed max-w-[min(100vw,1920px)]"
+      >
+    </Transition>
   </section>
 </template>
+<style scoped>
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 1s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+</style>
