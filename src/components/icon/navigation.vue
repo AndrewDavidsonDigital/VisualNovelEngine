72af24ea7ff4c0a670e3e7d0446a772fc2bcfeaa
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-07-02 21:25:40 +1000

    Engine:  - Add more icons

---
+++ src/components/icon/navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg" 
+    data-icon="navigation"
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    stroke-width="1" 
+    stroke-linecap="round" 
+    stroke-linejoin="round" 
+  >
+    <polygon 
+      data-layer="outter"
+      points="
+        21 11 
+        3 2 
+        11 21 
+        13 13 
+        21 11
+      "
+    ></polygon>
+    <polygon 
+      data-layer="inner"
+      stroke-width="0.5" 
+      points="
+        16 10 
+        6 5 
+        10.5 16 
+        11 11 
+        16 10
+      "
+    ></polygon>
+  </svg>
+</template>
