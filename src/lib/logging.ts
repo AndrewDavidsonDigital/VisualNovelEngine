84dcdea39193560aae7b853a95fe3cbe3e24602f
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-07-18 00:11:05 +1000

    Engine:  - Update logger to have a 2nd tier of logging and seperate toggle, `debug`

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -1,14 +1,20 @@
 interface ILoggingConfig {
   trace: boolean;
+  debug: boolean;
 }
 
 const KEY = 'illusionEngine_logging';
 
-const loggingConfig = (JSON.parse(window.localStorage.getItem(KEY) || '{ "trace": true }')) as ILoggingConfig;
+const loggingConfig = (JSON.parse(window.localStorage.getItem(KEY) || '{ "trace": true, "debug": true }')) as ILoggingConfig;
 window.localStorage.setItem(KEY, JSON.stringify(loggingConfig));
 
 export function trace(message: string){  
   if (loggingConfig.trace){
+    console.log(`${Date.now()} ${message}`);
+  }
+}
+export function debug(message: string){  
+  if (loggingConfig.debug){
     console.log(`${Date.now()} ${message}`);
   }
 }
