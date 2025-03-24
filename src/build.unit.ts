536575e5ff7d7708f8d6faded97809000b8908c9
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-03-25 00:20:51 +1100

    Engine: Expand build compliance to also cover NOT running with bun or pnpm

--- src/build.unit.ts
+++ src/build.unit.ts
@@ -1,13 +1,27 @@
-import { expect, test } from 'vitest'
+import { expect, test, beforeAll } from 'vitest'
 import fs from 'fs';
 
+let rootFolderContents: string[];
+
+beforeAll(() => {
+  rootFolderContents = fs.readdirSync('./', 'utf-8');
+});
+
 /**
- * Ensure `package-lock.json`, generated via npm install, doesn't exist
+ * Ensure no JavaScript bundler lock files exist in root dir
+ * This enforces using yarn as the package manager
  */
-test('Ensure no NPM compiled file in root dir', () => {
+test('Ensure no npm lock file exists', () => {
   const fileThatShouldNotExist = 'package-lock.json';
-
-  const rootFolderContents = fs.readdirSync('./', 'utf-8');
-
   expect(rootFolderContents.indexOf(fileThatShouldNotExist)).toBe(-1);
-})
+});
+
+test('Ensure no pnpm lock file exists', () => {
+  const fileThatShouldNotExist = 'pnpm-lock.yaml';
+  expect(rootFolderContents.indexOf(fileThatShouldNotExist)).toBe(-1);
+});
+
+test('Ensure no bun lock file exists', () => {
+  const fileThatShouldNotExist = 'bun.lockb';
+  expect(rootFolderContents.indexOf(fileThatShouldNotExist)).toBe(-1);
+});
