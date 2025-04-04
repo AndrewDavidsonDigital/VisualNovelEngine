348690bd8c95a127142ed0fa840098263a7c637e
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-04-04 20:57:54 +1100

    UX: Rienforce the un-interactable nature of `load` /  `continue` when there are no saves

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -25,9 +25,9 @@
   <nav class="flex flex-col gap-5 interactable-styling">
     <Clickable><RouterLink to="/new-game" >New Game</RouterLink></Clickable>
     <Clickable v-if="hasSaves"><RouterLink to="/continue" >Continue</RouterLink></Clickable>
-    <span v-else><a class="italic">Continue</a></span>
+    <span v-else class="pointer-events-none text-gray-300"><a class="italic">Continue</a></span>
     <Clickable v-if="hasSaves"><RouterLink to="/menu">Load Game</RouterLink></Clickable>
-    <span v-else><a class="italic">NYI: Load Game</a></span>
+    <span v-else class="pointer-events-none text-gray-300"><a class="italic">NYI: Load Game</a></span>
     <Clickable><RouterLink to="/options" >Options</RouterLink></Clickable>
     <Clickable><RouterLink to="/exit" >Exit</RouterLink></Clickable>
   </nav>
